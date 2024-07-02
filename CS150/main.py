5239fa14566df64655fd6a5af62193af8f163598
suchigowda31 <suchithragowdadpg@gmail.com>
2024-07-02 16:15:05 +0530

    resolved

--- CS150/main.py
+++ CS150/main.py
@@ -6,9 +6,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 1
-    b = 2
-
+    a = 30
+    b = 5
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
